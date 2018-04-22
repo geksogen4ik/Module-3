8c96043a8d1b4af2d8bee4f6adc707e6414d48ca
Svetlana <geksogen4ik@gmail.com>
2018-04-22 15:50:42 +0300

    HW

---
+++ HW/HW/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    class Dictionary
+    {
+
+        public static Dictionary<int, string> MyDictionary(int amount)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            Console.WriteLine("Enter the name: \n");
+            string name;
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write("Name:", i);
+                name = Console.ReadLine();
+                dic.Add(i, name);
+                Console.Clear();
+            }
+            return dic;
+        }
+        public static Dictionary<int, string> Delete(int amount)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            while (amount >= 3)
+            {
+                for (int i = 0; i < amount - 1; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        dic.Remove(i);
+                        Console.WriteLine($"ID: {i} is removed");
+                        amount--;
+                        //}
+                        //else{
+                        //    Console.WriteLine($"{i} is not removed");
+                        //}
+                    }
+                }
+            }
+                return dic;
+
+            }
+
+        }
+    }
+
+
---
+++ HW/HW/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many guys do you want to add? ");
+            try
+            {
+                int amount = int.Parse(Console.ReadLine());
+                Dictionary<int, string> dic = Dictionary.MyDictionary(amount);
+
+                // Получаем коллекцию ключей
+                ICollection<int> keys = dic.Keys;
+
+                Console.WriteLine("Collection conteins: ");
+                foreach (int items in keys)
+                    Console.WriteLine("ID: {0}  Name: {1}", items, dic[items]);
+
+
+                Console.WriteLine("delete process is starting");
+                Dictionary<int, string> dic1 = Dictionary.Delete(amount);
+                ICollection<int> keys1 = dic1.Keys;
+
+                Console.WriteLine("Upon deliting collection conteins: ");
+                foreach (int items in keys1)
+                    Console.WriteLine("ID: {0}  Name: {1}", items, dic1[items]);
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
+        
