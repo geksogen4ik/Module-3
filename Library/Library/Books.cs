6c02d8aebcac138d04e1b15a8d4a68be1edcdc23
Svetlana <geksogen4ik@gmail.com>
2018-04-28 22:10:49 +0300

    Start

--- Library/Library/Books.cs
+++ Library/Library/Books.cs
@@ -21,19 +21,19 @@ namespace Library
         }
 
         public BooksName bookName;
-        public DateTime term;
 
-        public void LibraryPopulating(int choice)
+        public void LibraryPopulating()
         {
             
             Console.WriteLine("What type of Book do you want to add into the Library?");
             Console.WriteLine("If you want add 'Book', select - [1]");
-            Console.WriteLine("If you want add 'Periodical', select - [1]");
+            Console.WriteLine("If you want add 'Periodical', select - [2]");
             Console.WriteLine("If you want add 'MultivolumeEdition', select - [3]");
             Console.WriteLine("If you want add 'Magazine', select - [4]");
             Console.WriteLine("If you want add 'Monograph', select - [5]");
             Console.WriteLine("If you want add 'Manuscript', select - [6]");
 
+            int choice = 0;
             Int32.TryParse(Console.ReadLine(), out choice);
 
             switch (choice)
